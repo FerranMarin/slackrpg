4bc5fd9b8e9af4fc866646deb74a7fd0923a781c
Ferran Marin <ferran.marin.llobet@gmail.com>
2015-11-12 18:08:06 +0100

    starting

--- slackbot_ferransito.py
+++ slackbot_ferransito.py
@@ -1,2 +1,15 @@
+import time
+from slackclient import SlackClient
+
+token = "xoxb-14434545828-lJTOdN7fEUWFj5kShiqjm46C"
+sc = SlackClient(token)
+if sc.rtm_connect():
+    while True:
+        print sc.rtm_read()
+        time.sleep(1)
+else:
+    print "Connection Failed, invalid token?"
 
 
+
+#general C0CTKKUAX
